12965d7349a2f312c4e92f2e2a66f1df9f232aa1
Adeeb Bin Islam <adeebbinislam@gmail.com>
2021-05-11 23:38:18 -0700

    Singleton Pattern

---
+++ singleton/singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Singleton pattern
+            // One object is created and can be reusable several times.
+
+            /*
+            Console.WriteLine("---  Singleton Pattern ---");
+            singleton_fn obj1 = singleton_fn.GetInstance();
+            singleton_fn obj2 = singleton_fn.GetInstance();
+            */
+
+
+        }
+    }
+}
---
+++ singleton/singleton/singleton_fn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    class singleton_fn
+    {
+        private static singleton_fn single;
+        private singleton_fn() {
+
+        }
+        
+        public static singleton_fn GetInstance(){
+            //var value;
+             if (single == null) {
+                Console.WriteLine("New value");
+                single = new singleton_fn();
+             }
+            return single;
+        }
+    }
+    
+}
